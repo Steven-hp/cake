fa6010313a1387c5dc6c48c286f61550b9bde4de
chenzy382111 <71804151+chenzy382111@users.noreply.github.com>
2020-11-18 01:29:59 +0800

    订单页首次提交

---
+++ Order.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-container> 
+      <!-- 顶部物流配送信息文本框     -->
+      <div class="adressInfo">配送信息</div>
+  </el-container>
+</template>
+<style scoped>
+.el-container{
+   width: 850px;
+   margin: 0 auto;
+}
+    .adressInfo{
+        border:1px solid #ddd;
+        height: 50px;
+        border-radius: 0px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, .12)
+    }
+</style>
+<script>
+</script>
