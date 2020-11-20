88b00d7a847fe2ca7455cda376e973527accbaf3
wenghuiyu <71804225+wenghuiyu@users.noreply.github.com>
2020-11-20 19:48:35 +0800

    最终登录页面及找回密码页面
    最终登录页面及找回密码页面

--- Login.vue
+++ Login.vue
@@ -1,62 +1,212 @@
 <template>
-  <!-- 整个页面的div-->
-  <div>
-    <!-- 给整个界面div添加背景图 -->
-    <div class="container note" :style="note">
+  <!-- 给整个界面div添加背景图 -->
+  <div class="container" :style="note">
+    <div class="cont_box">
       <!-- 登录界面的div开始 -->
       <div id="log_div">
         <!-- 选择登录方式的div开始 -->
         <div>
-          <ul style="list-style:none;display:flex;">
-            <li class="log_change">账号密码登录</li>
+          <ul @click="logtype" style="list-style: none; display: flex">
+            <li class="log_change lognumber">账号密码登录</li>
             |
-            <li class="log_change">手机验证码登录</li>
+            <li class="log_change logphone">手机验证码登录</li>
           </ul>
         </div>
         <!-- 选择登录方式的div结束 -->
-        <!-- 账号框，密码框开始 -->
+        <!-- 账号框，密码框,登录按钮开始 -->
         <div>
-          <div><input class="my_input" type="text" placeholder="请输入您的账号" value="" ></div>
-          <div><input class="my_input" type="text" placeholder="请输入您的账号" value="" ></div>
-          <div><button class="my_btn" type="button">登录</button></div>
-
+          <div class="number">
+            <div>
+              <input
+                class="my_input"
+                type="text"
+                placeholder="请输入您的账号"
+                name="logName"
+                v-model="uname"
+              />
+            </div>
+            <div>
+              <input
+                class="my_input"
+                type="password"
+                placeholder="请输入您的账号"
+                name="password"
+                v-model="upwd"
+              />
+            </div>
+          </div>
+          <div class="phone">
+            <!-- 手机验证登录手机号输入框 -->
+            <div>
+              <input
+                class="my_input"
+                type="text"
+                placeholder="请输入您的手机号"
+                name="log_phoneNumber"
+              />
+            </div>
+            <div>
+              <input
+                class="my_phoneInput"
+                type="text"
+                placeholder="短信验证"
+                name="log_number"
+              />
+              <input class="my_btn1" type="submit" value="短信验证码" />
+            </div>
+          </div>
+          <div>
+            <div id="tip" class="close">
+              <div class="log_err">用户名或密码错误</div>
+            </div>
+          </div>
+          <div>
+            <input class="my_btn" type="submit" value="登录" @click="handle" />
+          </div>
         </div>
-        <!-- 账号框，密码框结束 -->
+        <!-- 账号框，密码框,登录按钮结束 -->
+        <!-- 登录框底部开始 -->
+        <div class="log_bottom">
+          <div class="rememberMe">
+            <input
+              id="aaa"
+              type="checkbox"
+              checked
+              name="rememberMe"
+              value="记住账号"
+            />
+            <label for="aaa">&nbsp;记住账号</label>
+          </div>
+          <router-link to="/passport_lost" class="jump"
+            ><a href="#" class="a1">忘记密码</a></router-link
+          >
+          <a href="#" class="a2">去注册</a>
+        </div>
+        <!-- 登录框底部结束 -->
       </div>
       <!-- 登录界面的div结束 -->
     </div>
   </div>
-  
 </template>
 <style scoped>
-  /* 背景图 */
-  .container{
-    width: 100%;
-    height: 1000px;
-  }
-  /* 登录框大小及边框 */
-  #log_div{
-    width: 388px;
-    height: 356px;
-    border: 1px solid lightgray;
-    padding: 28px 42px;
-  }
-  .log_change{
-    font-size: 18px;
-    margin-right: 25px;
-    margin-left: 20px;
-  }
-  .my_input{
-    width: 302px;
-    height: 38px;
-  }
-  .my_btn{
-    width: 302px;
-    height: 48px;
-    background-color: #442818;
-    
-    padding: 0px 5px;
-  }
+/* 背景图 */
+.container {
+  width: 100%;
+  height: 91vh;
+}
+.cont_box {
+  width: 1000px;
+  height: 91vh;
+  margin: 0 auto;
+  position: relative;
+}
+/* 登录框大小及边框 */
+#log_div {
+  width: 388px;
+  height: 356px;
+  border: 1px solid #efefef;
+  padding: 28px 42px;
+  position: absolute;
+  top: 90px;
+  right: -120px;
+}
+.log_change {
+  font-size: 18px;
+  margin-right: 25px;
+  margin-left: 20px;
+  margin-bottom: 20px;
+  color: #482618;
+}
+.my_input {
+  width: 302px;
+  height: 35px;
+  padding: 5px;
+  margin-bottom: 15px;
+  box-sizing: border-box;
+}
+.my_input:focus {
+  outline: none;
+}
+.my_btn {
+  width: 314px;
+  height: 48px;
+  background-color: #442818;
+  border-radius: 2px;
+  padding: 0px 5px;
+  color: #ffffff;
+  font-size: 18px;
+  border: 0px;
+  margin-top: 30px;
+}
+.my_btn:focus {
+  outline: none;
+}
+.rememberMe {
+  cursor: pointer;
+  margin-left: 34px;
+  display: flex;
+}
+.log_bottom {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 15px;
+  font-size: 12px;
+  vertical-align: middle;
+  color: #744f3a;
+}
+.jump {
+  text-decoration: none;
+}
+.a1 {
+  margin-left: 120px;
+  color: #744f3a;
+  text-decoration: none;
+}
+.a2 {
+  margin-right: 37px;
+  text-decoration: none;
+  color: #744f3a;
+}
+/* 切换登录方式 */
+.phone {
+  display: none;
+}
+.my_phoneInput {
+  box-sizing: border-box;
+  width: 140px;
+  height: 35px;
+  padding: 3px;
+}
+.my_phoneInput:focus {
+  outline: none;
+}
+.my_btn1 {
+  box-sizing: border-box;
+  width: 151px;
+  height: 35px;
+  border: 1px solid rgb(231, 229, 229);
+  background-color: #fafafa;
+  outline: 0;
+  margin-left: 12px;
+  margin-bottom: 15px;
+  color: #442818;
+}
+.log_err {
+  background: url("../assets/gantan.png") no-repeat 1px -1px;
+  background-size: 18px;
+  position: absolute;
+  padding-left: 20px;
+  height: 17px;
+  left: 86px;
+  font-size: 12px;
+  color: red;
+}
+.open {
+  display: block;
+}
+.close {
+  display: none;
+}
 </style>
 <script>
 export default {
@@ -65,13 +215,77 @@ export default {
       note: {
         backgroundImage: "url(" + require("../assets/log_bg.jpg") + ")",
         backgroundRepeat: "no-repeat",
-        // backgroundSize: "25px auto",
         marginTop: "5px",
       },
+      //用户名密码的变量
+      uname: "",
+      upwd: "",
     };
   },
-  // methods: {
-  
-  // },
+  methods: {
+    logtype(e) {
+      if (e.target.className == "log_change lognumber") {
+        document.getElementsByClassName("number")[0].style.display = "block";
+        document.getElementsByClassName("phone")[0].style.display = "none";
+      } else if (e.target.className == "log_change logphone") {
+        document.getElementsByClassName("number")[0].style.display = "none";
+        document.getElementsByClassName("phone")[0].style.display = "block";
+      }
+    },
+    //单击注册按钮时校验表单
+    handle() {
+      //会引发短路现象
+      if (this.checkUname() && this.checkUpwd()) {
+        //发送相关的用户名和密码到web服务器
+        this.axios
+          .post("/login", "uname=" + this.uname + "&upwd=" + this.upwd)
+          .then((res) => {
+            //代表用户登录成功
+            if (res.data.code == 1) {
+              alert("正确");
+            } else {
+              console.log("用户名或密码错误");
+            }
+          });
+      }
+    },
+    //检测用户名
+    checkUname() {
+      let uname = this.uname;
+      let unameReg = /^[0-9a-zA-Z]{6,12}$/;
+      if (unameReg.test(uname)) {
+        return true;
+      } else {
+        //显示短消息提示框(标准写法)
+        console.log("用户名错误");
+        document.getElementById("tip").className = "open";
+        //终止函数的执行
+        return false;
+      }
+    },
+    //检测密码
+    checkUpwd() {
+      let upwd = this.upwd;
+      let upwdReg = /^[0-9A-Za-z\.\-_]{8,15}$/;
+      if (upwdReg.test(upwd)) {
+        return true;
+      } else {
+        console.log("密码错误");
+        document.getElementById("tip").className = "open";
+      }
+    },
+  },
+  watch: {
+    uname() {
+      if (this.uname == "") {
+        document.getElementById("tip").className = "close";
+      }
+    },
+    upwd() {
+      if (this.upwd == "") {
+        document.getElementById("tip").className = "close";
+      }
+    },
+  },
 };
 </script>
--- Passport_lost.vue
+++ Passport_lost.vue
@@ -1,20 +1,153 @@
 <!-- 忘记密码 -->
 <template>
-    <div>
-        <p class="el-icon-user">找回密码</p>
+  <div>
+    <!-- 标题 -->
+    <div id="title">
+      <h2>找回密码</h2>
     </div>
-    <!-- 找回密码框 -->
-    <!-- <div>
-        <el-radio v-model="radio" label="1">手机</el-radio>
-        <el-radio v-model="radio" label="2">邮箱</el-radio>
-    </div> -->
+    <!-- 操作框 -->
+    <div id="frame">
+      <!-- 单选框开始 -->
+      <div class="change">
+        <label class="phone"
+          ><input type="radio" name="change" value="手机" checked />手机</label
+        >
+        <label class="email"
+          ><input type="radio" name="change" value="邮箱" />邮箱</label
+        >
+      </div>
+      <!-- 单选框结束 -->
+      <!-- 输入框开始 -->
+      <div>
+        <div>
+          <input
+            class="input"
+            type="text"
+            placeholder="请输入您的手机号"
+            name="phone"
+          />
+        </div>
+        <div class="div_input">
+          <input
+            class="input1"
+            type="text"
+            placeholder="填写图片验证码"
+            name="picturenNumber"
+          />
+          <canvas
+            id="canvas"
+            style="border: 1px solid red; width: 80px; height: 20px"
+          ></canvas>
+          <span id="yanzhengma"></span
+          ><a class="a" @click="getVerification">看不清?换一张</a>
+        </div>
+        <div><input class="btn" type="submit" value="下一步" /></div>
+      </div>
+      <!-- 输入框结束 -->
+    </div>
+  </div>
 </template>
+<style scoped>
+h2 {
+  margin: 0;
+}
+#title {
+  border: none;
+  padding-left: 150px;
+  height: 60px;
+  line-height: 60px;
+  color: #8d6a54;
+  background: url(../assets/people.png) no-repeat 95px;
+  text-align: left;
+  margin-top: 70px;
+}
+.phone,
+.email {
+  font-size: 16px;
+  color: #604136;
+  padding: 0 10px;
+}
+.change {
+  padding-bottom: 30px;
+}
+.input,
+.div_input {
+  width: 398px;
+  height: 30px;
+  padding: 16px 0px 16px 20px;
+  color: #9e8266;
+  border: 0.5px solid #dedfdf;
+}
+.div_input {
+  display: inline-block;
+}
+.input1 {
+  margin-left: -80px;
+  border: 0;
+}
+.input1:focus {
+  outline: 0;
+}
+.a {
+  font-size: 12px;
+  color: rgb(51, 118, 243);
+  margin-left: 20px;
+}
+.input:focus {
+  outline: 0;
+}
+.btn {
+  width: 422px;
+  height: 55px;
+  font-size: 18px;
+  color: #fff;
+  border: 0px;
+  background-color: #491903;
+  margin-top: 30px;
+}
+.btn:focus {
+  outline: 0;
+}
+</style>
 <script>
-  export default {
-    data () {
-      return {
-        radio: '1'
-      };
-    }
-  }
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    randomColor() {
+      var colorValue = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f";
+      var colorArray = colorValue.split(",");
+      var color = "#";
+      for (var i = 0; i < 6; i++) {
+        color += colorArray[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    },
+    getRandomNum(n) {
+      return parseInt(Math.random() * n);
+    },
+    getVerification() {
+      var ctx = document.getElementById("canvas").getContext("2d");
+      ctx.clearRect(0, 0, 400, 400);
+      // 设置字体
+      ctx.font = "128px bold 黑体";
+      // 设置垂直对齐方式
+      ctx.textBaseline = "top";
+      // 设置颜色
+      ctx.fillStyle = this.randomColor();
+      // 绘制文字（参数：要写的字，x坐标，y坐标）
+      ctx.fillText(this.getRandomNum(10), 0, this.getRandomNum(50));
+      ctx.fillStyle = this.randomColor();
+      ctx.fillText(this.getRandomNum(10), 50, this.getRandomNum(50));
+      ctx.fillStyle = this.randomColor();
+      ctx.fillText(this.getRandomNum(10), 100, this.getRandomNum(50));
+      ctx.fillStyle = this.randomColor();
+      ctx.fillText(this.getRandomNum(10), 150, this.getRandomNum(50));
+    },
+  },
+  mounted() {
+    this.getVerification();
+  },
+};
 </script>
